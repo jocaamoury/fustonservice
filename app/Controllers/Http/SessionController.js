333f72482e40059136b27a52c399d5c5b861ed93
joao <joao_amoury@hotmail.com>
2022-01-26 12:47:11 -0300

    login parcial

---
+++ app/Controllers/Http/SessionController.js
@@ -0,0 +1,20 @@
+'use strict'
+
+class SessionController {
+    async login ({request, response, auth, session}){
+        const {email, password} = request.all()
+        try{
+            await auth.attempt(email , password)
+            return response.redirect('/inicio')
+        }catch(error){
+            session.flash({
+                message: 'Autenticação não pode ser efetuada',
+                type: 'danger'
+            })
+            return response.redirect('/login')
+        }
+        console.log(email,password)
+    }
+}
+
+module.exports = SessionController
